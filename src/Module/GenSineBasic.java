c63b3536a328d23a726bbe971bd2f60ad22d657c
viennetp <viennetp@D7010M25126.phelma.grenoble-inp.fr>
2017-04-05 14:25:27 +0200

    nettoie

--- src/Module/GenSineBasic.java
+++ src/Module/GenSineBasic.java
@@ -1,27 +1,29 @@
 
-public class GenSineBasic extend ModuleAbstract{
-
+public class GenSineBasic extends ModuleAbstract{
+    private double PI = 3.1415926;
     private double frequency;
     private double amplitude;
     private int n;
     private double newSample;
 
     public GenSineBasic(String nom, double f, double a){
-        super("GenSineBasic", 0, 1);
+        super(nom, 0, 1);
         this.frequency = f;
         this.amplitude = a;
+        this.n = 0;
     }
 
+    public int getN(){
+        return this.n;
+    }
 
     @Override
-    public void exec(){
-
+     public void exec(){
         // Calcul du nouveau sample et mise à jour de l'indice de temps
         this.newSample = this.amplitude*Math.sin(2*PI*this.frequency*n/this.SAMPLE_FREQ);
         this.n = this.n + 1;
         // Aller on envoi
-        this.outputPorts[0].setValue(this.newSample);
-        this.outputPorts[0].connexion.communicate();
+        this.setAndSendOutputPortValue(0,this.newSample);
 
 
     }
